1f2d8bcf2bce855b8eca93035dc21b58f33ce6c7
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-10-20 13:42:08 +0800

    fix: ci fix
    fix ci and
    
    Log: fix ci

--- try_compile/try_compile.cpp
+++ try_compile/try_compile.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2020 - 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 #include <private/qquickstyleplugin_p.h>
 
 int main (int argc, char *argv[])
