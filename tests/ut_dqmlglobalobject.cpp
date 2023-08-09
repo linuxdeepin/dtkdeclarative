a35a7a9d173d200269096679b35b7b68ae8c299d
wangfei <wangfei@deepin.org>
2023-08-09 14:01:04 +0800

    chore: Complementary unit test ut-globalobject
    unit test
    
    Log: Complementary unit test ut-globalobject

--- tests/ut_dqmlglobalobject.cpp
+++ tests/ut_dqmlglobalobject.cpp
@@ -8,6 +8,7 @@
 
 #include <QPointer>
 #include <QPalette>
+#include <QSignalSpy>
 
 #include <DPalette>
 #include <DFontManager>
@@ -15,6 +16,7 @@
 #include <DGuiApplicationHelper>
 
 #include <private/dqmlglobalobject_p.h>
+#include "dmessagemanager_p.h"
 
 DQUICK_USE_NAMESPACE
 DGUI_USE_NAMESPACE
@@ -98,6 +100,19 @@ TEST_F(ut_DQMLGlobalObject, palette)
     }
 }
 
+TEST_F(ut_DQMLGlobalObject, paletteChanged)
+{
+    Q_UNUSED(pObj->palette());
+
+    DPalette oldPalatte = pAppIns->applicationPalette();
+    DPalette newPalatte = oldPalatte;
+    newPalatte.setColor(DPalette::TextTitle, Qt::red);
+    QSignalSpy spy(pObj, &DQMLGlobalObject::paletteChanged);
+    pAppIns->setApplicationPalette(newPalatte);
+    EXPECT_EQ(spy.count(), 1);
+    pAppIns->setApplicationPalette(oldPalatte);
+}
+
 TEST_F(ut_DQMLGlobalObject, inactivePalette)
 {
     DPalette expected = pAppIns->applicationPalette();
@@ -123,6 +138,62 @@ TEST_F(ut_DQMLGlobalObject, makeColor)
     ASSERT_EQ(color1.toColor(pt), pt.highlight().color());
 }
 
+TEST_F(ut_DQMLGlobalObject, deepinWebsite)
+{
+    ASSERT_TRUE(!pObj->deepinWebsiteName().isEmpty());
+    ASSERT_TRUE(!pObj->deepinWebsiteLink().isEmpty());
+}
+
+TEST_F(ut_DQMLGlobalObject, deepinDistributionOrgLogo)
+{
+    ASSERT_TRUE(!pObj->deepinDistributionOrgLogo().isEmpty());
+}
+
+TEST_F(ut_DQMLGlobalObject, blendColor)
+{
+    QColor color = pObj->blendColor(Qt::red, Qt::blue);
+    EXPECT_NE(color, QColor(Qt::red));
+    EXPECT_EQ(color, QColor(Qt::blue));
+}
+
+TEST_F(ut_DQMLGlobalObject, toColorType)
+{
+    DGuiApplicationHelper::ColorType colorType = pObj->toColorType(pAppIns->applicationPalette().window().color());
+    EXPECT_EQ(colorType, pObj->themeType());
+}
+
+TEST_F(ut_DQMLGlobalObject, selectColor)
+{
+    QColor color = pObj->selectColor(pAppIns->applicationPalette().window().color(), Qt::white, Qt::black);
+    QColor expectColor = pObj->themeType() == DGuiApplicationHelper::DarkType ? QColor(Qt::black) : QColor(Qt::white);
+    EXPECT_EQ(color, expectColor);
+}
+
+TEST_F(ut_DQMLGlobalObject, sendMessage)
+{
+    ControlHelper<> helper("qrc:/qml/QmlGlobalObject_MessageManager.qml");
+    ASSERT_TRUE(helper.object);
+
+    EXPECT_TRUE(pObj->sendMessage(helper.object, "msg1", "icon1", 1));
+}
+
+TEST_F(ut_DQMLGlobalObject, sendMessageByDelegate)
+{
+    ControlHelper<QQuickWindow> helper("qrc:/qml/QmlGlobalObject_MessageManager.qml");
+    QQuickWindow *root = helper.object;
+    MessageManager *msgMgr = qobject_cast<MessageManager *>(qmlAttachedPropertiesObject<MessageManager>(root, false));
+    ASSERT_TRUE(msgMgr);
+    QQmlComponent *delegate = root->property("containerCom").value<QQmlComponent *>();
+    ASSERT_TRUE(delegate);
+    QVariantMap argus;
+    argus.insert("msg", QStringList{"msg1"});
+    argus.insert("icon", QStringList{"icon1"});
+    EXPECT_TRUE(pObj->sendMessage(root, delegate, argus, 1, "id1"));
+    EXPECT_EQ(msgMgr->count(), 1);
+    pObj->closeMessage(root, "id1");
+    EXPECT_EQ(msgMgr->count(), 0);
+}
+
 TEST(ut_DColor, construct)
 {
     DColor invalidColor;
@@ -137,6 +208,13 @@ TEST(ut_DColor, construct)
 
     ASSERT_EQ(color.toColor(pt), Qt::red);
 
+    DColor color1(DColor::HighlightedText);
+    ASSERT_TRUE(color1.isTypedColor());
+    ASSERT_TRUE(color1.isValid());
+
+    pt.setBrush(QPalette::HighlightedText, Qt::blue);
+    ASSERT_EQ(color1.toColor(pt), Qt::blue);
+
     DColor color2(QColor(Qt::red));
     ASSERT_EQ(color2.color(), Qt::red);
 }
@@ -150,4 +228,13 @@ TEST(ut_DColor, common)
     EXPECT_NE(color.data.opacity, color2.data.opacity);
     EXPECT_NE(color.data.saturation, color2.data.saturation);
     EXPECT_NE(color.data.lightness, color2.data.lightness);
+}
+
+TEST(ut_DColor, operatorEQ)
+{
+    DColor color1(QColor(Qt::red));
+    DColor color2(QColor(Qt::blue));
+    DColor color3(QColor(Qt::blue));
+    EXPECT_NE(color1, color2);
+    EXPECT_EQ(color2, color3);
 }
