linuxdeepin/dtkdeclarative

commit 7b58695d3745a30b91ff29367729ef6dc24f7086

fix: Wrong DConfigWrapper's property value and signal
  Add ut-test for undefined property.
  Fix some error for DConfigWrapper.

src/private/dconfigwrapper.cpp
@@ -98,9 +98,6 @@ DConfigWrapper::DConfigWrapper(QObject *parent)
 
 DConfigWrapper::~DConfigWrapper()
 {
-    if (impl) {
-        impl->deleteLater();
-    }
 }
 
 /*!
@@ -244,9 +241,16 @@ void DConfigWrapper::componentComplete()
     for (auto iter = properties.begin(); iter != properties.end(); iter++) {
         // it's need to emit signal, because other qml object maybe read the old value
         // when binding the property before the component completed, also it has a performance problem.
-        mo->setValue(iter.key(), impl->value(iter.key(), iter.value()));
+        if (iter.value().isNull()) {
+            // sync backend's value to `Wrapper`
+            mo->setValue(iter.key(), impl->value(iter.key()));
+        } else {
+            // sync Wrapper's value(defined in qml) to backend.
+            setProperty(iter.key(), iter.value());
+        }
     }
 
+     // Using QueuedConnection because impl->setValue maybe emit sync signal in `propertyWriteValue`.
     connect(impl, &DTK_CORE_NAMESPACE::DConfig::valueChanged, this, [this, mo, properties](const QString &key){
         const QByteArray &proName = key.toLocal8Bit();
         if (properties.contains(proName)) {
@@ -254,5 +258,5 @@ void DConfigWrapper::componentComplete()
             mo->setValue(proName, impl->value(proName, properties.value(proName)));
         }
         Q_EMIT valueChanged(key);
-    });
+    }, Qt::QueuedConnection);
 }

tests/ut_dconfigwrapper.cpp
@@ -16,9 +16,8 @@ static constexpr char const *FILE_NAME = "example";
 class ut_DConfigWrapper : public ::testing::Test
 {
 public:
-    virtual void SetUp();
-
-    static void SetUpTestCase() {
+    virtual void SetUp()
+    {
         const QString &target = QString("%1/usr/share/dsg/configs/%2/%3.json").arg(LocalPrefix, APP_ID, FILE_NAME);
 
         if (!QFile::exists(QFileInfo(target).path()))
@@ -30,34 +29,38 @@ public:
         qputenv("DSG_DCONFIG_BACKEND_TYPE", "FileBackend");
         qputenv("DSG_DATA_DIRS", "/usr/share/dsg");
     }
-    static void TearDownTestCase() {
+    virtual void TearDown() {
         QDir(LocalPrefix).removeRecursively();
     }
-
-    ControlHelper<QQuickItem> helper;
-    DConfigWrapper *config;
-    QString origiAppId;
 };
 
-void ut_DConfigWrapper::SetUp()
+TEST_F(ut_DConfigWrapper, componentComplete)
 {
-    ASSERT_TRUE(helper.load("qrc:/qml/Config.qml"));
-    config = helper.object->findChild<DConfigWrapper *>();
-    ASSERT_TRUE(config);
+    QScopedPointer<DConfigWrapper> config(new DConfigWrapper());
+
+    config->classBegin();
+    config->setName("example");
+    config->setSubpath("");
+    config->componentComplete();
+
     ASSERT_TRUE(config->isValid());
 }
 
 TEST_F(ut_DConfigWrapper, setValue)
 {
-    ASSERT_EQ(config->name(), "example");
-    config->setName("example1");
-    ASSERT_EQ(config->name(), "example");
+    QScopedPointer<DConfigWrapper> config(new DConfigWrapper());
 
-    ASSERT_EQ(config->subpath(), "");
+    config->classBegin();
+    config->setName("example");
+    config->componentComplete();
+
+    ASSERT_TRUE(config->isValid());
+
+    EXPECT_EQ(config->subpath(), "");
 
     ASSERT_TRUE(config->keyList().contains("key3"));
 
-    QSignalSpy spy(config, &DConfigWrapper::valueChanged);
+    QSignalSpy spy(config.data(), &DConfigWrapper::valueChanged);
 
     QString newValue = config->value("key3", QString("default")).toString() + QString("abc");
     config->setValue("key3", newValue);
@@ -75,8 +78,21 @@ TEST_F(ut_DConfigWrapper, setValue)
     ASSERT_EQ(config->value("key3").toString(), "application");
 }
 
-TEST_F(ut_DConfigWrapper, setValueByQML)
+TEST_F(ut_DConfigWrapper, setValueByQml)
 {
+    ControlHelper<QQuickItem> helper("qrc:/qml/Config.qml");
+    ASSERT_TRUE(helper.object);
+
+    DConfigWrapper *config = helper.object->findChild<DConfigWrapper *>();
+    ASSERT_TRUE(config);
+    ASSERT_TRUE(config->isValid());
+
+    EXPECT_EQ(config->property("key2"), "key2");
+    EXPECT_EQ(config->value("key2"), "key2");
+
+    EXPECT_EQ(config->property("key3"), "1");
+    EXPECT_EQ(config->value("key3"), "1");
+
     QSignalSpy key3Change(helper.object, SIGNAL(key3Changed()));
     config->metaObject()->invokeMethod(helper.object, "setKey3", Q_ARG(QVariant, QString("2")));
     EXPECT_EQ(key3Change.count(), 1);

tests/qml/Config.qml
@@ -12,6 +12,7 @@ Item {
         id: exampleConfig
         name: "example"
         subpath: ""
+        property string key2
         property string key3 : "1"
         onKey3Changed: control.key3Changed()
     }

commit 92cb58c315635b7c88baa2807dd68f8f58a73563

fix: Wrong DConfigWrapper's initialized value
  It introduced in 7b58695d3745a30b91ff29367729ef6dc24f7086
We only use Wrapper's value as fallback value instead of current value.
it is different between qt5 with qt6 for `property string key`,
in qt5, it's value read in QMetaProperty is false for `isNull`, but
it's true in qt6.

src/private/dconfigwrapper.cpp
@@ -241,13 +241,8 @@ void DConfigWrapper::componentComplete()
     for (auto iter = properties.begin(); iter != properties.end(); iter++) {
         // it's need to emit signal, because other qml object maybe read the old value
         // when binding the property before the component completed, also it has a performance problem.
-        if (iter.value().isNull()) {
-            // sync backend's value to `Wrapper`
-            mo->setValue(iter.key(), impl->value(iter.key()));
-        } else {
-            // sync Wrapper's value(defined in qml) to backend.
-            setProperty(iter.key(), iter.value());
-        }
+        // sync backend's value to `Wrapper`, we only use Wrapper's value(defined in qml) as fallback value.
+        mo->setValue(iter.key(), impl->value(iter.key(), iter.value()));
     }
 
      // Using QueuedConnection because impl->setValue maybe emit sync signal in `propertyWriteValue`.

tests/ut_dconfigwrapper.cpp
@@ -90,8 +90,9 @@ TEST_F(ut_DConfigWrapper, setValueByQml)
     EXPECT_EQ(config->property("key2"), "key2");
     EXPECT_EQ(config->value("key2"), "key2");
 
-    EXPECT_EQ(config->property("key3"), "1");
-    EXPECT_EQ(config->value("key3"), "1");
+    // `key3`'s value(defined in qml) is only fallback value instead of current value.
+    EXPECT_EQ(config->property("key3"), "application");
+    EXPECT_EQ(config->value("key3"), "application");
 
     QSignalSpy key3Change(helper.object, SIGNAL(key3Changed()));
     config->metaObject()->invokeMethod(helper.object, "setKey3", Q_ARG(QVariant, QString("2")));